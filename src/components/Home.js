531c079cc2a4f629504d4c69b4c81e5f9d823c62
Lukasz Mroz <lukasz.mroz@merck.com>
2022-05-18 08:17:19 +0200

    Add hooks examples

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Link, useNavigate, Outlet } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthStatus } from '../utils/auth.js';
+import { useAuth } from '../utils/auth.js';
 
 export const Home = () => {
   let navigate = useNavigate();
+  let [count, setCount] = useState(0);
+  const location = useLocation();
+  const authContext = useAuth();
   return (
     <div>
       <h1>Homepage</h1>
+      <p>{`Count: ${count}`}</p>
+      <button onClick={() => setCount((count) => count + 1)}>Count +</button>
+      <button onClick={() => setCount((count) => count - 1)}>Count -</button>
+      <p>{`Location: ${location.pathname}`}</p>
+      <p>{`AuthContext user: ${authContext.user}`}</p>
       <AuthStatus />
       <nav>
         <ul>
